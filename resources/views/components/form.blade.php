6e4e1d67139cfe3816cd49ad4bf19091559efb83
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-06-27 16:07:34 +0200

    Move form to top

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,7 +1,6 @@
 <div class="vpt-signup-form vpt-section" id="petition">
     <div class="vpt-signup-form__container">
         <div class="fdb-signup-form__inner">
-            <h2 class="text-xl md:text-3xl font-bold mb-4">{{__("vpt.form.title")}}</h2>
             <form action="/supporters" method="POST" class="vpt-form mt-6">
                 @foreach (__("vpt.form.fields") as $field)
                     <x-form-field :name="$field['name']" :label="$field['label']" :placeholder="$field['placeholder'] ?? ''" :required="$field['required']" :type="$field['type']" :checked="$field['checked'] ?? false" :classes="$field['class'] ?? false" />
@@ -9,7 +8,7 @@
                 @csrf
                 <input type="hidden" name="source" value="{{$_COOKIE['sr_source'] ?? ""}}">
                 <input type="hidden" name="locale" value="{{app()->getLocale()}}">
-                <button type="submit" class="vpt-button w-full flex justify-center items-center gap-x-2 mt-2 font-bold">{{__("vpt.form.submit") }}  <i class="icofont-duotone icofont-apply"></i></button>
+                <button type="submit" class="vpt-button !bg-white !text-primary w-full flex justify-center items-center gap-x-2 mt-2 font-bold">{{__("vpt.form.submit") }}  <i class="icofont-duotone icofont-apply"></i></button>
             </form>
         </div>
     </div>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -15,10 +15,10 @@ $parser = new Illuminate\Support\Str;
         </div>
         @endif
         <h1 class="text-3xl md:text-4xl lg:text-5xl text-center vpt-adieu mb-4">Volksentscheide respektieren!</h1>
-        <div class="vpt-card mt-12 mb-20">
+        <div class="vpt-card my-12">
             <h2 class="text-2xl font-bold mb-4">Akzeptieren Sie die Demokratie,<br>Frau Barandun und Herr Anwander!</h2>
             <p class="mb-4">Wir haben abgestimmt. Und wir haben gewonnen. Und doch können es die Rechten nicht lassen: Mit haltlosen und unnützen Rekursen versuchen sie die Einführung des Mindestlohns in den grössten Zürcher Städten zu verzögern oder gar zu verhindern. <b>So funktioniert unsere Demokratie nicht!</b> Mit diesem Brief fordern wir den Gewerbeverband und die Arbeitgebervereinigung dazu auf, ihre Niederlage zu akzeptieren.</p>
-            <a href="#sign" class="vpt-button block w-full !bg-white !text-primary">Unterschreibe auch! <i class="icofont-duotone icofont-apply"></i></a>
+            <x-form />
         </div>
         <div class="vpt-addressants flex gap-12 md:flex-nowrap flex-wrap mb-8">
             <div class="vpt-addressant w-1/2">
@@ -43,9 +43,6 @@ $parser = new Illuminate\Support\Str;
         <x-initial-signer />
         <p id="show-all-signers" class="underline cursor-pointer mt-4 mb-12">und {{ supporterCount() - 10 }} weitere Unterzeichner*innen</p>
         <x-logos />
-        <div id="sign">
-            <x-form />
-        </div>
         <x-social-share/>
     </div>
 </x-app-layout>
